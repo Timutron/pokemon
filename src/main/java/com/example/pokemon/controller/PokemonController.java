7e321df73d19053227bc6fd9fc3cd526a8617af2
Timutron <Timutron@mail.ru>
2019-09-04 15:48:24 +0400

    Добавлено логирование

--- src/main/java/com/example/pokemon/controller/PokemonController.java
+++ src/main/java/com/example/pokemon/controller/PokemonController.java
@@ -2,6 +2,7 @@ package com.example.pokemon.controller;
 
 import com.example.pokemon.service.PokemonDeserializationService;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.io.IOException;
 
 @RestController
+@Slf4j
 public class PokemonController {
 
     @Autowired
@@ -21,6 +23,7 @@ public class PokemonController {
     public ModelAndView getPokeList() throws IOException, UnirestException {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("pokemonList", pokeService.getPoke().getPokemonList());
+        log.info("ПЕРЕХОД НА СТРАНИЦУ СПИСКА ПОКЕМОНОВ");
         return modelAndView;
     }
 }
--- src/main/java/com/example/pokemon/controller/RegistrationController.java
+++ src/main/java/com/example/pokemon/controller/RegistrationController.java
@@ -2,12 +2,14 @@ package com.example.pokemon.controller;
 
 import com.example.pokemon.entity.User;
 import com.example.pokemon.service.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@Slf4j
 public class RegistrationController {
 
     @Autowired
@@ -19,8 +21,10 @@ public class RegistrationController {
         User userFromDB = IUserService.findByLogin(user.getLogin());
         if (userFromDB != null){
             modelAndView.addObject("message", "Такой пользователь уже существует");
+            log.info("Попытка создания зарегестрированного пользователя");
         }
         IUserService.save(user);
+        log.info("Создан новый пользователь");
         modelAndView.setViewName("registration");
         return modelAndView;
     }
