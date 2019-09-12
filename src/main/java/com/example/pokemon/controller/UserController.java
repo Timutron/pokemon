2568b3283c8bfb6b477d9e08b4a9c57b0c2068f4
Timutron <Timutron@mail.ru>
2019-09-13 00:11:30 +0400

    добавлена работа с PagedResourcesAssembler

--- src/main/java/com/example/pokemon/controller/UserController.java
+++ src/main/java/com/example/pokemon/controller/UserController.java
@@ -2,26 +2,30 @@ package com.example.pokemon.controller;
 
 import com.example.pokemon.entity.User;
 import com.example.pokemon.entity.UserResource;
-import com.example.pokemon.service.IUserService;
 import com.example.pokemon.hateoas.UserResourceAssembler;
+import com.example.pokemon.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
-import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users", produces = MediaTypes.HAL_JSON_VALUE)
 public class UserController {
 
     @Autowired
     private IUserService iUserService;
     @Autowired
     private UserResourceAssembler resourceAssembler;
+    @Autowired
+    private PagedResourcesAssembler<User> pagedResourcesAssembler;
 
     @GetMapping("/{userId}")
     public UserResource getUserById(@PathVariable Integer userId){
@@ -29,9 +33,9 @@ public class UserController {
         return resourceAssembler.toResource(user);
     }
 
-    @GetMapping("/all")
-    public PagedResources<UserResource> getAllUser(Pageable pageable, PagedResourcesAssembler<User> pagedResourcesAssembler){
-        Page<User> users = iUserService.findAll(PageRequest.of(0,5));
+    @GetMapping(value = "/all")
+    public PagedResources<UserResource> getAllUser(@PageableDefault Pageable pageable){
+        Page<User> users = iUserService.findAll(pageable);
         return pagedResourcesAssembler.toResource(users, resourceAssembler);
     }
 }
